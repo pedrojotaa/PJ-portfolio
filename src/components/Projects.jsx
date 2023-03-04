356d09fffaaccf6e2f43a0581aa6679b7c8958dc
“João <eng.joao.pedro@outlook.com>
2023-03-04 09:04:01 -0300

    refactor: building project component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import project1 from "../assets/1.png";
 
 function Projects() {
   return (
-    <div>Projects</div>
-  )
+    <Wrapper className="section">
+      <div className="section-center">
+        <div className="container">
+          <h2>project name</h2>
+          <img src={project1} alt="p1" />
+          <p>description</p>
+        </div>
+        <div className="container">
+          <h2>project name</h2>
+          <img src={project1} alt="p1" />
+          <p>description</p>
+        </div>
+      </div>
+    </Wrapper>
+  );
 }
 
-export default Projects
+const Wrapper = styled.section`
+  display: grid;
+  gap: 2rem;
+  img {
+    width: 100%;
+    display: block;
+    object-fit: cover;
+    height: 250px;
+    border-radius: 5px;
+    border: 1px solid #222;
+  }
+`;
+
+export default Projects;
